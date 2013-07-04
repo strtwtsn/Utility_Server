c4128baf7b0616d929013f3865d1087ef60db4e2
strtwtsn <stuart.watson@unep-wcmc.org>
2013-07-04 16:05:52 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get -y update
-apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-dev libyaml-dev git
+apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-dev libyaml-dev git checkinstall
 cd /usr/local/src
 wget ftp://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p195.tar.gz
 tar -xvzf ruby-2.0.0-p195.tar.gz
@@ -8,4 +8,5 @@ cd /usr/local/src/ruby-2.0.0-p195/
 ./configure
 make
 sudo checkinstall --pkgname ruby_2.0 --pkgversion 1.0 --default
+gem update --system 1.8.25
 /usr/local/bin/gem install bundler chef ruby-shadow --no-ri --no-rdoc
