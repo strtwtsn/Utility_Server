3428df4fd8135c61dda1559fbcc764ce15cd85c1
strtwtsn <stuart.watson@unep-wcmc.org>
2013-07-05 09:22:47 +0100

    Update default.rb

--- cookbooks/postgres/recipes/default.rb
+++ cookbooks/postgres/recipes/default.rb
@@ -1,20 +1,20 @@
-#template "/etc/apt/sources.list.d/pgsql.list" do
-#source "pgsql.list.erb"
-#end
+template "/etc/apt/sources.list.d/pgsql.list" do
+source "pgsql.list.erb"
+end
 
-#bash "Install postgresql client" do
-#user "root"
-#code <<-EOH
-#wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
-#sudo apt-key add -
-#sudo apt-get update
-#sudo apt-get install -y postgresql-9.2 postgresql-server-dev-9.2
-#EOH
-#end
+bash "Install postgresql client" do
+user "root"
+code <<-EOH
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
+sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y postgresql-9.2 postgresql-server-dev-9.2
+EOH
+end
 
-#gem_package "pg" do
-#action :install
-#end
+gem_package "pg" do
+action :install
+end
 
 service "postgresql" do
   service_name "postgresql"
